2c1a4cdfa1e44f5067be47d25c1d9bf5836e1a96
gregofigueras <65877630+gregofigueras@users.noreply.github.com>
2024-11-06 16:10:07 -0300

    Update AllTests.java
    .

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestAuto.class, TestCombi.class, TestChoferPermanente.class, TestChoferTemporario.class, TestCombi.class,TestMoto.class,TestPedido.class })
+@SuiteClasses({ TestAuto.class, TestCombi.class, TestChoferPermanente.class, TestChoferTemporario.class, TestCombi.class,TestMoto.class,TestPedido.class, FalsoOptionPane.class, GuiTestAdmin.class, GuiTestCliente.class, GuiTestLogin.class, GuiTestPanelRegistro.class, })
 public class AllTests {
 
 }
